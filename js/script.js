826ae9500a5b06dd8b0707f43b259a2abb47444f
Andrzej Glomski <andrzejglomski2@gmail.com>
2020-12-11 12:57:57 +0100

    Add class active function Js

--- js/script.js
+++ js/script.js
@@ -6,30 +6,34 @@
 
 
   const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log("link was clicked!");
-    console.log(event)
-  }
-   /* remove class 'active' from all article links  */
+    console.log(event);
+    
+ 
+   /* [DONE]remove class 'active' from all article links  */
    const activeLinks = document.querySelectorAll('.titles a.active');
 
    for(let activeLink of activeLinks){
      activeLink.classList.remove('active');
    }
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS]add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
 
-  /* remove class 'active' from all articles */
+  /*[DONE] remove class 'active' from all articles 
   const activeArticles = document.querySelectorAll('.titles a.active');
 
   for(let activeArticle of activeArticles){
-    activeLink.classList.remove('active');
-  }
+    activeArticle.classList.remove('active');
+  }*/
+  
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
-
-
+  }
   const links = document.querySelectorAll('.titles a');
   
   for(let link of links){
